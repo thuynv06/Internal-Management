b0d9e967ddb022cd22c81d82750adba0571a0ef6
diepnv <diepit.nv@gmail.com>
2018-05-16 14:22:51 +0700

    Initial Project

--- IFIPageViewV2/src/App/App.jsx
+++ IFIPageViewV2/src/App/App.jsx
@@ -1,5 +1,28 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import styles from './css/style.css';
+
+
 
 class App extends React.Component {
+
+    render() {
+        return (
+                <div className={styles.container_wrap}>
+                <div className="container ">
+                        Hello World
+                   </div>
+              </div>
+        );
+    }
 }
+
+function mapStateToProps(state) {
+    const { alert } = state;
+    return {
+        alert
+    };
+}
+
+const connectedApp = connect(mapStateToProps)(App);
 export { connectedApp as App };
