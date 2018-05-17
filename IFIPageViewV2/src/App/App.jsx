1cb4eef5cffce15563ed5a72129850427a20d444
diepnv <diepit.nv@gmail.com>
2018-05-17 16:54:56 +0700

    Button + Input

--- IFIPageViewV2/src/App/App.jsx
+++ IFIPageViewV2/src/App/App.jsx
@@ -2,22 +2,30 @@ import React from 'react';
 import { connect } from 'react-redux';
 import styles from './css/style.css';
 import InputComponent from '../_components/InputComponent';
+import Button from '../_components/Button';
 
 
 
 class App extends React.Component {
+    
 
     render() {
+        const styleButton = {
+            height: "40px", color: "orange", width: "10%" ,fontsize: "20px" ,background:"green" ,borderRadius:"10px"
+        };
         return (
                 <div className={styles.container_wrap}>
                     <div className="container ">
-                        <InputComponent name="diep" type="password" value="diep@gmail.com" placeholder="Input"/>
+                        Username
+                        <InputComponent name="diep" type="text" value="root" placeholder="Username" icon="fa fa-user"/>
+                        Password
+                        <InputComponent name="diep" type="password" value="123564" placeholder="Password" icon="fa fa-unlock"/>
                     </div>
-
+                    <Button type="submit" {...styleButton}>
+                        Login
+                    </Button>
                 </div>
-                <Button type="submit" height="100px" color="orange" width="100%" fontsize="20px" background="green" borderRadius="10px">
-                  Login
-                </Button>
+                
         );
     }
 }
