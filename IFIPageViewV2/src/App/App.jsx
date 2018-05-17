3e26a07dbc2f8a807c2b3c5b1428aa68993ba6af
diepnv <diepit.nv@gmail.com>
2018-05-17 14:40:27 +0700

    Update InputComponent

--- IFIPageViewV2/src/App/App.jsx
+++ IFIPageViewV2/src/App/App.jsx
@@ -2,16 +2,31 @@ import React from 'react';
 import { connect } from 'react-redux';
 import styles from './css/style.css';
 import InputComponent from '../_components/InputComponent';
+import FontAwesome from 'react-fontawesome';
 
 
 
 class App extends React.Component {
+    constructor(props){
+        super(props);
+        this.changText = this.changText.bind(this);
+    }
+
+    changText(e){
+        const value = e.target.value;
+        this.setState({ value });
+    
+        console.log(value + ' hehe');
+    }
 
     render() {
         return (
                 <div className={styles.container_wrap}>
-                    <div className="container ">
-                        <InputComponent name="diep" type="password" value="diep@gmail.com" placeholder="Input"/>
+                    <div className="container">
+                        Username
+                        <InputComponent name="diep" type="text" value="diep" placeholder="Username" onChange={this.changText} icon="fa fa-user"/>
+                        Password
+                        <InputComponent name="nv" type="password" value="nv" placeholder="Password" onChange={this.changText} icon="fa fa-unlock"/>
                     </div>
                 </div>
         );
